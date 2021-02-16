f7503283033d543c5b564dd3104f4477bfa8cc91
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-16 13:12:58 +0000

    Initialize backend server

---
+++ backend/server/index.js
@@ -0,0 +1,16 @@
+// http is an inbuilt module in Node.js
+const http = require('http');
+
+// createServer method takes a callback function as argument
+// the callback function takes two arguments req and re
+const server = http.createServer(function (req, res) {
+    res.statusCode = 200; // 200 = OK
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<h1>Hunger free</h1>");
+    res.end();
+});
+
+// server is listening to incoming requests on port 4000 on localhost
+server.listen(4000, function () {
+    console.log("Listening on port http://localhost:4000");
+});
