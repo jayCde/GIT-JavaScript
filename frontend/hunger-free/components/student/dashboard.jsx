95b633c3edc3a7ca19702aa7749f43e824d6b34b
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-21 23:24:27 +0000

    Add base skeleton for dashboard component

---
+++ frontend/hunger-free/components/student/dashboard.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import Head from 'next/head';
+import Link from 'next/Link';
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/student.module.css';
+
+
+
+
+export default function StudentDashboard(){
+
+//Student state object
+const [firstname, setstudentFirstname] = useState("");
+const [lastname, setstudentLastname] = useState("");
+const [username, setstudentUsername] = useState("");
+
+//UseEffect function to fetch student details and check login status
+    useEffect(() => {
+        const studentdata = localStorage.getItem('student');
+
+        if (studentdata){
+            const foundstudent = JSON.parse(studentdata);
+            setstudentFirstname(foundstudent.firstname);
+            setstudentLastname(foundstudent.lastname);
+            setstudentUsername(foundstudent.username);
+
+            console.log('Student is logged in');
+            console.log("Student data is", foundstudent)
+        
+        } else {
+            window.location.href="login"
+        }
+    }, [])
+
+    return(
+
+        <div className={styles.container}>
+            <Head>
+                <title>Hunger-Free kids :: Student Dashboard</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        
+            <main className={styles.main}>
+                <div>
+                    <Link href="login"><button>Logout</button></Link>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                &#169; The Healthy, Hunger-Free kids - 2021.
+            </footer>
+        </div>
+    )
+}
