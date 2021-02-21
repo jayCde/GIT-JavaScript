fd3299f44a9b64de3030b577a3ad9c63571abbe8
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-21 22:31:31 +0000

    Complete login fucntionality

--- frontend/hunger-free/components/login.jsx
+++ frontend/hunger-free/components/login.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import Head from 'next/head'
-// import Link from 'next/Link'
-// import React, { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/Link';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
-import { API_BASE_URL, API_PORT} from '../../config'
-import styles from '../styles/landing.module.css'
+// import { Redirect } from "react-router-dom";
+import styles from '../styles/landing.module.css';
+import { API_BASE_URL, API_PORT} from '../../config';
+import isAlphanumeric from 'validator/lib/isAlphanumeric';
+
 
 
 export default function Login(){
@@ -20,8 +23,39 @@ export default function Login(){
 
     //Endpoint(s) and url(s)
 
-    const endpoint = '/admin';
-    const url = `${API_BASE_URL}:${API_PORT}/${endpoint}`;
+    const endpoint = 'students';
+    const studentuserurl = `${API_BASE_URL}:${API_PORT}/${endpoint}`;
+
+    //Handle form data for login
+    const onSubmit = ( data = {username, password}) =>{
+        console.log('data is', data)
+        axios.get(studentuserurl, {
+            username: data.username,
+            password: data.password
+        }).then (resp =>{
+
+            //If user credentials are correct and login successful
+            if (resp.status == 200){
+                window.location="../pages/studentsdashboard";
+                alert("Login Successful")
+                console.log(resp.data)
+                localStorage.setItem('student', JSON.stringify(resp.data))
+            }else{
+                //if user credentials are inccorect and login unsuccessful
+                window.location.href="login";
+                console.log(resp.data);
+            }
+        }).catch(err =>{
+            console.log("An error occurred while logging in", err)
+            alert("Kindly enter valid credentials")
+        })
+        console.log(data)
+    }
+
+    //watch errors in form fields
+    console.log(watch('username', 'password'));
+    console.log(errors.username, errors.password)
+ 
 
     return(
 
@@ -37,28 +71,41 @@ export default function Login(){
                 </h1>
 
                 <div className={styles.card}>
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
                         <input  type="text" 
                                 className={styles.InputField}
                                 placeholder="Username"
                                 name="username"
-                                id="username">
-                                {/* ref={register({required: true, minLength: 8})} */}
-                        </input>
+                                id="username"
+                                ref={register({required: true, minLength: 8,
+                                    validate: (input) => isAlphanumeric (input)
+                                })}
+                        />
+                        {errors.username && <span className={styles.errors}>Kindly enter a valid username</span> } <br/>
+                        <br></br>
 
+                        <br></br>
 
                         <input type="password" 
                                 className={styles.InputField}
                                 placeholder="Password"
                                 name="password"
-                                id="password">
-                                {/* ref={register({required: true, minLength: 8})} */}
-                        </input>
-
+                                id="password"
+                                ref={register({required: true, minLength: 8,
+                                    validate: (input) => isAlphanumeric (input)
+                                })}
+                        />
+                        {errors.password && <span className={styles.errors}>Kindly enter a valid password</span> } <br/>
+                        <br></br>
+                        
                         <button type="submit" value="submit" className={styles.button}>Login</button>
 
                     </form>
+
+                    <br></br>
+
+                    <Link href="signup"><button>Don't Have an Account? Sign Up Here</button></Link>
                 </div>
             </main>
 
