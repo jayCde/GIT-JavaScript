0fdbe587fdf048aee99f2475bc50d1342eb4f596
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-22 08:03:12 +0000

    Add admin login button

--- frontend/hunger-free/components/login.jsx
+++ frontend/hunger-free/components/login.jsx
@@ -12,7 +12,7 @@ import isAlphanumeric from 'validator/lib/isAlphanumeric';
 
 export default function Login(){
 
-    //react hooks
+    //Using react hooks
 
     const { 
         register, errors, watch,
@@ -106,6 +106,7 @@ export default function Login(){
                     <br></br>
 
                     <Link href="signup"><button>Don't Have an Account? Sign Up Here</button></Link>
+                    <Link href="admin"><button>Admin Login</button></Link>
                 </div>
             </main>
 
