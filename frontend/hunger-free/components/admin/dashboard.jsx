6522e391e98eb3bf4afc37d28d71cb5ee6d4a1b7
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-24 14:57:18 +0000

    Change imports, add page sections

--- frontend/hunger-free/components/admin/dashboard.jsx
+++ frontend/hunger-free/components/admin/dashboard.jsx
@@ -1,37 +1,47 @@
 import Head from 'next/head';
 import Link from 'next/Link';
+import styles from '../../styles/admindashboard.module.css';
 
 export default function Admindashboard() {
     return(
-        <div>
+        <div className={styles.container}>
             <Head>
-                <div>
+                <title>Hunger-Free :: Admin Dashboard</title>
+                <link rel="icon" href="/favicon/"></link>
+            </Head>
+
+            <main className={styles.main}>
+                <div className={styles.title}>
                     <h3>
                         Welcome to the admin dashboard, 
                     </h3>
                 </div>
-            </Head>
 
-            <main>
+                <div className={styles.card}>
+                    <div className={styles.users}>
+                        Add, delete and update users here.
 
-                <div>
-                    Add, delete and update users here.
+                        <button> Add user</button>
+                    </div>
+
+                    <div className={styles.orders}>
+                        Orders for the day
+                        <hr style={{borderColor: "black", backgroundColor: "black",width: "70%", height: "2px"}}></hr>
+
+                    </div>
+
+
+                    <div className={styles.meals}>
+                        Add, delete and update meals
+                        <button>Add meal</button>
+                    </div>
                 </div>
-
-                <div>
-                    Add, update and delete meals here.
-                </div>
-
-                <div>
-                    Orders for the day
-                </div>
-
                 <div>
                     <Link href="/"><button>Logout</button></Link>
                 </div>
             </main>
 
-            <footer>
+            <footer className={styles.footer}>
             &#169; The Healthy, Hunger-Free kids - 2021.
 
             </footer>
