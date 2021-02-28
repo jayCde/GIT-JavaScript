5f19be6783d19cae4617f22083c7670d9b788c08
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-28 16:29:36 +0000

    Add meal menu skeleton

---
+++ frontend/hunger-free/components/admin/menumgmt/menu.jsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+// import Link from 'next/Link';
+import styles from '../../styles/admindashboard.module.css';
+
+export default function Menumgmt() {
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Hunger-Free :: Admin Dashboard</title>
+                <link rel="icon" href="/favicon/"></link>
+            </Head>
+
+            <main className={styles.main} id="main">
+                <div className={styles.referencelinks} id="reference links">
+
+                    {/* <img src="/public/1.jpg" alt="logo"/> */}
+                    <a href="#" className={styles.logo}/>
+
+                    <a href="../../../pages/admindashboard/users" className={styles.userlink} >
+                        Users
+                    </a>
+                    <a href="#meals" className={styles.menulink} >
+                        Menu
+                    </a>
+                    <a href="../../../pages/admindashboard/#orders" className={styles.orderlink} >
+                        Orders
+                    </a>
+
+                    <a href="/" className={styles.logout}>Logout</a>
+
+                </div>
+
+                <div className={styles.title}>
+                    <h3>
+                        Manage the menus for all meal here.
+                    </h3>
+                </div>
+
+                <div className={styles.card} id="card">
+
+                    <div className={styles.meals} id="meals">
+                        Manage meal menu                        
+                    </div>
+
+                </div>
+
+            </main>
+
+            <footer className={styles.footer}>
+            &#169; The Healthy, Hunger-Free kids - 2021.
+
+            </footer>
+        </div>
+    )
+}
