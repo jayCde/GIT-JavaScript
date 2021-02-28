09ac098613341ac6b3c8e0f4340f836a1fd93782
jayCde <68590879+jayCde@users.noreply.github.com>
2021-02-28 16:29:57 +0000

    Add users management skeleton

---
+++ frontend/hunger-free/components/admin/usersmgmt/users.jsx
@@ -0,0 +1,56 @@
+import Head from 'next/head';
+// import Link from 'next/Link';
+import styles from '../../styles/admindashboard.module.css';
+
+export default function Usermgmt() {
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Hunger-Free :: Admin Dashboard</title>
+                <link rel="icon" href="/favicon/"></link>
+            </Head>
+
+            <main className={styles.main} id="main">
+                <div className={styles.referencelinks} id="reference links">
+
+                    {/* <img src="/public/1.jpg" alt="logo"/> */}
+                    <a href="#" className={styles.logo}/>
+
+                    <a href="#users" className={styles.userlink} >
+                        Users
+                    </a>
+                    <a href="../../../pages/admindashboard/menu" className={styles.menulink} >
+                        Menu
+                    </a>
+                    <a href="../../../pages/admindashboard/#orders" className={styles.orderlink} >
+                        Orders
+                    </a>
+
+                    <a href="/" className={styles.logout}>Logout</a>
+
+                </div>
+
+                <div className={styles.title}>
+                    <h3>
+                        Welcome to the admin dashboard, 
+                    </h3>
+                </div>
+
+                <div className={styles.card} id="card">
+
+                    <div className={styles.users} id="users">
+                        Manage users here
+                        <button> Manage users</button>
+                    </div>
+
+                </div>
+
+            </main>
+
+            <footer className={styles.footer}>
+            &#169; The Healthy, Hunger-Free kids - 2021.
+
+            </footer>
+        </div>
+    )
+}
