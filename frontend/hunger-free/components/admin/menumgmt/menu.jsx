c4376775c987994e3cdaa028cf4e6f2b1bc23757
jayCde <68590879+jayCde@users.noreply.github.com>
2021-03-10 23:12:11 +0000

    Add function to add meal to menu in database

--- frontend/hunger-free/components/admin/menumgmt/menu.jsx
+++ frontend/hunger-free/components/admin/menumgmt/menu.jsx
@@ -1,8 +1,57 @@
 import Head from 'next/head';
+import axios from 'axios'
+import { useForm } from "react-hook-form";
+import isAlpha from 'validator/lib/isAlpha';
 // import Link from 'next/Link';
+import {API_BASE_URL, API_PORT} from '../../../../config';
 import styles from '../../../styles/admin/admindashboard.module.css';
 
 export default function Menumgmt() {
+
+    //react hooks
+    const { 
+        register, errors, watch,
+            handleSubmit = async (e) => {
+            {e.preventDefault}
+            }  
+        } = useForm ();
+
+        //Endpoint(s) and url(s)
+
+        const endpoint = 'meal';
+        const menuurl = `${API_BASE_URL}:${API_PORT}/${endpoint}`;
+    
+        //Handle form data for meal addition
+        const onSubmit = ( data = {food}) =>{
+
+            console.log('data is', data)
+            axios.post(menuurl, {
+                id: data.id,
+                food: data.food,
+            }).then (resp =>{
+    
+                //If new meal addition is successful
+                if (resp.status == 201){
+                    window.location="/menu";
+                    alert("The meal has been added to menu successfully")
+                    console.log(resp.data)
+                }else{
+                    //if credentials entered for user addition are incorrect and addition unsuccessful
+                    window.location.href="/users";
+                    console.log(resp.data);
+                }
+            }).catch(err =>{
+                console.log("An error occurred while adding meal to menu", err)
+                alert("Sorry an error occurred while adding a meal to the menu.Kindly try again later.")
+            })
+            console.log(data)
+        }
+    
+        //watch errors in form fields
+        console.log(watch('food'));
+        console.log(errors.food)
+
+
     return(
         <div className={styles.container}>
             <Head>
@@ -39,7 +88,32 @@ export default function Menumgmt() {
                 <div className={styles.card} id="card">
 
                     <div className={styles.meals} id="meals">
-                        Manage meal menu                        
+                        Manage meal menu       
+
+                        <hr style={{borderColor: "black", backgroundColor: "black",width: "70%", height: "2px"}}></hr>
+
+                        <div className={styles.addusers}>Add meal
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input  type="text" 
+                                    className={styles.InputField}
+                                    placeholder="Enter meal to add to menu here"
+                                    name="food"
+                                    id="food"
+                                    ref={register({required: true,
+                                        validate: (input) => isAlpha (input)
+                                    })}
+                                />
+                                <br></br>
+                                {errors.food && <span className={styles.errors}>Enter a meal to add to menu, It should be at least 4 characters.</span> } <br/>
+                                <br></br>
+
+                                <center>
+                                    <button type="submit" value="submit" className={styles.userbtn}>Add Meal</button>
+                                </center>
+
+                            </form>
+                        </div>
+                                        
                     </div>
 
                 </div>
