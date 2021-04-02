a4c2827436cac0e63965484374b7d64ee7154e23
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-04-02 01:51:46 +0000

    Bump in configuration file manually

---
+++ vhs-movie-hiring/vhs-mh/configuration-files/config.js
@@ -0,0 +1,15 @@
+const API_BASE_URL = "http://localhost";
+const API_PORT = 7000;
+const API_PORT_FRONTEND = 3000;
+
+const HOMEPAGE = 'landing'
+const LOGIN = 'login'
+const SIGNUP = 'signup'
+const STUDENT = 'student'
+const ADMIN = "admin"
+
+export {    API_BASE_URL, API_PORT, 
+            API_PORT_FRONTEND, 
+            HOMEPAGE, LOGIN,
+            SIGNUP, STUDENT, ADMIN
+}
